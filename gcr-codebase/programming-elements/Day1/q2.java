b1c424f740872c5c20ce135569d7f7e200338f11
KryptoKomal <kryp70girl@gmail.com>
2026-06-08 12:50:43 +0530

    day1

---
+++ gcr-codebase/programming-elements/Day1/q2.java
@@ -0,0 +1,16 @@
+//package BridgeLabz-SIPP-Training-2026.gcr-codebase.programming-elements.Day1;
+
+import java.util.Scanner;
+
+public class q2 {
+     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 1st number");
+        int n1=sc.nextInt();
+        System.out.println("enter 2nd number");
+        int n2=sc.nextInt();
+        int sum =(n1 + n2);
+        System.out.println(sum);
+     }
+    
+}
